a41fdd1d3a40b250f74896673413b5c3e680064f
tanaymehta28 <tanaymehta28@gmail.com>
2018-08-29 12:53:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+import random
+
 print("Hello There! \nWelcome to the Safe and Secure Register and Login system.\n")
 
 regId = str(input("Please Choose a username: \n"))
 regPass = str(input("Please choose a strong Password: \n"))
+
+random_seed = random.randint(88748,9879878794)
+
 
 while len(regId) == 0 and len(regPass) < 8:
     while type(regId) == int:
         regId = str(input("Please choose a valid UserId: "))
         regPass = str(input("Please enter a valid Password: "))
 
-regId = hash(regId)*83983973756
+
+#Generates a random seed to run everytime.
+
+regId = hash(regId)*random_seed
 regId = regId/57889
 
-regPass = hash(regPass)*939784868
+regPass = hash(regPass)*random_seed
 regPass = regPass+7265374
 
 
@@ -26,10 +34,10 @@ logId = str(input("Please enter your Login-id: \n"))
 
 logPass = str(input("Please enter your Login-Password: \n"))
 
-logId = hash(logId)*83983973756
+logId = hash(logId)*random_seed
 logId = logId/57889
 
-logPass = hash(logPass)*939784868
+logPass = hash(logPass)*random_seed
 logPass = logPass+7265374
 
 while logId != regId or logPass != regPass:
@@ -37,12 +45,13 @@ while logId != regId or logPass != regPass:
     logId = str(input("Please enter your Login-id: \n"))
     logPass = str(input("Please enter your Login-Password: \n"))
 
-    logId = hash(logId) * 83983973756
+    logId = hash(logId) * random_seed
     logId = logId / 57889
 
-    logPass = hash(logPass) * 939784868
+    logPass = hash(logPass) * random_seed
     logPass = logPass + 7265374
 
 print("Login Sucessfull! \n")
+
 
 
